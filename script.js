c43237deb4e33649d6c8c29426fa4ec763de492d
FawazAshrafAli <fawazashrafali@gmail.com>
2024-03-09 12:30:29 +0530

    KTM First commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function change(){
+    document.getElementById('duke-section').style.backgroundImage='url(https://s7g10.scene7.com/is/image/ktm/378263_sdrr-segment-header?wid=2000&hei=1334&dpr=off)';
+    
+    document.getElementById('duke-btn').style.marginLeft='5%';
+    document.getElementById('duke-btn').style.backgroundImage='linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(https://s7g10.scene7.com/is/image/ktm/150669_%20KTM%20390%20DUKE%20MY%202017:Medium?wid=700&hei=525&dpr=off)';
+    document.getElementById('duke-btn').style.transform='scale(0.001,0.001)';
+
+    document.getElementById('duke-btn-span').innerHTML='DUKE';
+    
+    document.getElementById('duke390-block').style.transform='scale(1,1)';
+    document.getElementById('duke250-block').style.transform='scale(1,1)';
+    document.getElementById('duke200-block').style.transform='scale(1,1)';
+    document.getElementById('duke125-block').style.transform='scale(1,1)';
+
+    
+    document.getElementById('duke250-block').style.transform='translate(117%, 0)','scale(1,1)';
+    document.getElementById('duke200-block').style.transform='translate(0, 125%)','scale(1,1)';
+    document.getElementById('duke125-block').style.transform='translate(117%, 125%)','scale(1,1)';
+}
+
